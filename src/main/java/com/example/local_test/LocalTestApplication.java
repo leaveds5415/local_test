a3438d0e3775efcb089cc0e266f0f9ea1d02b877
신상현 <sanghyeon.shin@sanghyeonshin-home.local>
2025-01-23 23:41:38 +0900

    first

---
+++ src/main/java/com/example/local_test/LocalTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.local_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LocalTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/local_test/LocalTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.local_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocalTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
